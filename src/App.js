cc97637092621227c48a12849c3dbbc2c00288c1
lfeliperibeiro <piporibeiro@gmail.com>
2020-10-09 16:05:32 -0300

    refactor: change readme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Layout from '../src/layout'
+import './app.css'
 
 
 
 function App() {
   return (
+    <>
    <Layout/>
+   <div className="footer">Feito com ❤️ por Felipe Ribeiro</div>
+   </>
   );
 }
 
