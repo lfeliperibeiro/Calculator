6504c64e9fbc2ba996761b22361a2abd4daee9ab
lfeliperibeiro <piporibeiro@gmail.com>
2020-10-09 15:53:43 -0300

    feat: create logic of calc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from '../src/layout'
 
 
 
 function App() {
   return (
-   <div>Hello</div>
+   <Layout/>
   );
 }
 
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import './style.css'
+import Button from '../components/button'
+import Display from '../components/display'
 
 const initialState = {
   displayValue: '0',
@@ -11,6 +14,7 @@ const initialState = {
 export default class Layout extends Component {
   state = {...initialState}
   constructor (props) {
+    super(props)
     this.clearMemory = this.clearMemory.bind(this)
     this.setOperation = this.setOperation.bind(this)
     this.addDigit = this.addDigit.bind(this)
@@ -21,26 +25,72 @@ export default class Layout extends Component {
 
   setOperation(operation){
     if (this.state.current === 0){
-      this.setState({operation, current: 1, clearDisplay: true})
-    }else {
-      const equals = operation === '='
-      const currentOperation = this.state.operation
+        this.setState({ operation, current: 1, clearDisplay:true })
+    }else{
+        const equals = operation === '='
+        const currentOperation = this.state.operation
+        const values = [...this.state.values]
+        try{
+            // eslint-disable-next-line no-eval
+            values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+        }catch(e){
+            values[0] = this.state.values[0]
+        }
+        values[1] = 0
+        this.setState({
+            displayValue: values[0],
+            operation: equals ? null : operation,
+            current: equals ? 0 : 1,
+            clearDisplay: !equals,
+            values
+        })           
+        
+    }
+}
+
+  addDigit(n) {
+    if (n === '.' && this.state.displayValue.includes('.')){
+      return 
+    }
+    const clearDisplay = this.state.displayValue === '0'
+    || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({displayValue, clearDisplay: false})
+
+    if (n !== '.'){
+      const i = this.state.current
+      const newValue = parseFloat(displayValue)
       const values = [...this.state.values]
-      try{
-        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
-      }catch(err) {
-        values[0] = this.state.values[0]
-      }
-      values[1] = 0
-      this.setState({
-        displayValue: values[0],
-        operation: equals ? null : operation,
-        current: equals ? 0 : 1,
-        clearDisplay: !equals,
-        values
-      })
+      values[i] = newValue
+      this.setState({values})
     }
   }
 
+  render () {
+    return (
+      <div className="calculator">
+                <Display value={this.state.displayValue} />
+                <Button label='AC' click={this.clearMemory} triple />
+                <Button label='/' click={this.setOperation} operation/>
+                <Button label='7' click={this.addDigit}/>
+                <Button label='8' click={this.addDigit}/>
+                <Button label='9' click={this.addDigit}/>
+                <Button label='*' click={this.setOperation} operation/>
+                <Button label='4' click={this.addDigit}/>
+                <Button label='5' click={this.addDigit}/>
+                <Button label='6' click={this.addDigit}/>
+                <Button label='-' click={this.setOperation} operation/>
+                <Button label='1' click={this.addDigit}/>
+                <Button label='2' click={this.addDigit}/>
+                <Button label='3' click={this.addDigit}/>
+                <Button label='+' click={this.setOperation} operation/>
+                <Button label='0' click={this.addDigit} double />
+                <Button label='.' click={this.addDigit}/>
+                <Button label='=' click={this.setOperation} operation/>
+                
+            </div>
+    )
+  }
  
 }
